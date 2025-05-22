01547805fbb7dcea37f46a9fb8b6baf8cdce4c47
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-05-22 15:42:16 +1200

    idk whats going on so i quit

---
+++ Demo/src/routes/pokemon/+page.js
@@ -0,0 +1,7 @@
+const url = "http://pokeapi.co/api/v2/pokemon/ditto";
+
+export async function load({ fetch }) {
+    const res = await fetch(url);
+    const poke = await res.json();
+    return { poke };
+  }
---
+++ Demo/src/routes/pokemon/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let { data } = $props();
+    console.log(data);
+
+</script>
+
+<h2>{data.poke.name}</h2>
+
+lala idk
